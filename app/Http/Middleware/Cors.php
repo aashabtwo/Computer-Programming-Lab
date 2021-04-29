c16ce8a8df80ffd2fccf7bb225a69fe40544c607
Tajwar <tajwaraashab@gmail.com>
2021-04-29 14:26:13 +0600

    cors port changed

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
+        ->header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Credentials', 'true');
     }
